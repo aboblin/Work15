2d54e091fe482dac676affba9884860982fe2949
aboblin <aboylan@stuy.edu>
2017-12-06 14:03:32 -0500

    added gcd(a,b)

--- funky_town.js
+++ funky_town.js
@@ -6,15 +6,21 @@ var fibonacci = function(n) {
     if (n > 1) {return n + fibonacci(n - 1);}
 };
 
+var gcd = function(a, b) {
+    if (a == b) {return a;}
+    if (a > b) {gcd(b,a - b);}
+    if (b > a) {gcd(a,b - a);}
+}
+
 var randomList = function(n, list){
-return list[n];
+    return list[n];
 };
 
 var randomNumber = function(len){
-return Math.floor(Math.random() * len);
+    return Math.floor(Math.random() * len);
 };
 
 var randomStudent = function(){
-var n = randomNumber(list.length);
-return randomList(n, list);
+    var n = randomNumber(list.length);
+    return randomList(n, list);
 };
