56b3e4fd8c46524a37b7e5bbc089c7405c5331f0
Dimitriy <dleksanov@stuy.edu>
2017-12-06 13:57:50 -0500

    added random

--- funky_town.js
+++ funky_town.js
@@ -1,5 +1,20 @@
+var list = ['jane', 'john', 'josh', 'jeff']
+
 var fibonacci = function(n) {
     if (n == 0) {return 0;}
     if (n == 1) {return 1;}
     if (n > 1) {return n + fibonacci(n - 1);}
+};
+
+var randomList = function(n, list){
+return list[n];
+};
+
+var randomNumber = function(len){
+return Math.floor(Math.random() * len);
+};
+
+var randomStudent = function(){
+var n = randomNumber(list.length);
+return randomList(n, list);
 };
